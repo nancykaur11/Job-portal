39255e06bdac930f2a736e35dcfa2a3c83f976d1
Nancy Kaur <nancykaur1119@gmail.com>
2026-01-29 19:22:48 +0530

    test the without serilisater code

--- accounts/views.py
+++ accounts/views.py
@@ -50,77 +50,72 @@ class LoginView(APIView):
 # from rest_framework import status
 # from django.contrib.auth import authenticate
 # from rest_framework_simplejwt.tokens import RefreshToken
-
 # from .models import User, Candidate, Recruiter
 
 
 # class RegisterView(APIView):
-#     permission_classes = [AllowAny]
+#     permission_classes=[AllowAny]
+    
+#     def post(self,request):
+#         email=request.data.get("email")
+#         name=request.data.get("name")
+#         role=request.data.get("role")
+#         password=request.data.get("password")
 
-#     def post(self, request):
-#         email = request.data.get("email")
-#         name = request.data.get("name")
-#         role = request.data.get("role")
-#         password = request.data.get("password")
-
-#         if not all([email, name, role, password]):
+#         if not all([email,name,role,password]):
 #             return Response(
-#                 {"error": "All fields are required"},
+#                 "All fields are required",
 #                 status=status.HTTP_400_BAD_REQUEST
 #             )
-
 #         if User.objects.filter(email=email).exists():
 #             return Response(
-#                 {"error": "Email already exists"},
+#                 "Email already exists",
 #                 status=status.HTTP_400_BAD_REQUEST
-#             )
-
-#         user = User.objects.create_user(
+#             )   
+            
+#         user=User.objects.create_user(
 #             email=email,
 #             name=name,
 #             role=role
-#         )
+#         )     
 #         user.set_password(password)
 #         user.save()
-
-#         if role == "CANDIDATE":
-#             Candidate.objects.create(user=user)
-#         elif role == "RECRUITER":
-#             Recruiter.objects.create(user=user, company_name="")
-
-#         refresh = RefreshToken.for_user(user)
-
+        
+#         if role=="CANDIDATE":
+#             Candidate.objects.create(user=user)     
+#         elif role=="RECRUITER":
+#             Recruiter.objects.create(user=user,company_name="")
+#         refresh=RefreshToken.for_user(user)
 #         return Response({
-#             "message": "User registered successfully",
-#             "access": str(refresh.access_token),
-#             "refresh": str(refresh)
-#         }, status=status.HTTP_201_CREATED)
+#             "message":"User registered successfully",
+#             "access":str(refresh.access_token),
+#             "refresh":str(refresh)
+#         },status=status.HTTP_201_CREATED) 
 
 
 # class LoginView(APIView):
 #     permission_classes = [AllowAny]
-
-#     def post(self, request):
-#         email = request.data.get("email")
-#         password = request.data.get("password")
-
+#     def post(self,request):
+#         email=request.data.get("email")
+#         password=request.data.get("password")
+        
 #         if not email or not password:
 #             return Response(
-#                 {"error": "Email and password required"},
-#                 status=status.HTTP_400_BAD_REQUEST
+#                 "Email and password required",
+#                 status= status.HTTP_400_BAD_REQUEST
 #             )
-
 #         user = authenticate(email=email, password=password)
-
 #         if not user:
 #             return Response(
-#                 {"error": "Invalid credentials"},
-#                 status=status.HTTP_401_UNAUTHORIZED
+#                 "wrong crendiatal",
+#                 status= status.HTTP_401_UNAUTHORIZED
 #             )
-
+        
 #         refresh = RefreshToken.for_user(user)
 
 #         return Response({
 #             "access": str(refresh.access_token),
 #             "refresh": str(refresh)
-#         })
+#         })         
+        
+        
